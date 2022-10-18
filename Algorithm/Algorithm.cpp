61d59abd44b1f54d66135b7f1ccce80bf393e721
khs <dlsgh1347@naver.com>
2022-10-19 01:36:56 +0900

    sort update

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -4,38 +4,129 @@
 #include <stack>
 #include <queue>
 using namespace std;
-#include "BinarySearchTree.h"
 #include <thread>
 
-//    [20]
-//[10]	   [30]
-//      [25]  [40]
-//				[50]
+// 정렬
+
+// C# 자료구조/알고리즘
+// -> A* OpenList (PQ)
+// -> C# List = C++ vector
+
+
+// PQ O(logN)
+// Red-Black Tree O(logN)
+// Sorting ?
+
+// 힙 정렬
+void HeapSort(vector<int>& v)
+{
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	// O(NlogN)
+	for (int num : v)
+		pq.push(num);
+
+	v.clear();
+
+	// O(NlogN)
+	while (pq.empty() == false)
+	{
+		v.push_back(pq.top());
+		pq.pop();
+	}
+}
+
+// 병합 정렬
+// 멀티 쓰레드 등 유리.
+// 분할 정복 (Divide and conquer)
+// - 분할 (Divide)		 문제를 더 단순하게 분할한다
+// - 정복 (Conquer)		 분할된 문제를 해결
+// - 결합 (Combine)		 결과를 취합하여 마무리
+
+// [3][K][7][2][J][4][8][9]				8개 * 1
+// [3][K][7][2] [J][4][8][9]			4개 * 2
+// [3][K] [7][2] [J][4] [8][9]			2개 * 4
+// [3] [K] [7] [2] [J] [4] [8] [9]		1개 * 8
+// [3][K] [2][7] [4][J] [8][9]			2개 * 4
+// [2][3][4][K] [4][8][9][J]			4개 * 2
+
+vector<int> Merge(vector<int> a, vector<int> b) // 크래프톤 손코딩 유사 문제, MergeResult 알고리즘.
+{
+	vector<int> temp;
+
+	return temp;
+}
+
+// O(NlogN)
+void MergeResult(vector<int>& v, int left, int mid, int right)
+{
+	// [2][3][7][K]    [4][8][9][J]
+	// [l]             [r]
+	int leftIdx = left;
+	int rightIdx = mid + 1;
+	
+	vector<int> temp;
+
+	while (leftIdx <= mid && rightIdx <= right)
+	{
+		if (v[leftIdx] <= v[rightIdx])
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+		else
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+	// 왼쪽이 먼저 끝났으면, 오른쪽 나머지 데이터 복사
+	if (leftIdx > mid)
+	{
+		while (rightIdx <= right)
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+	// 오른쪽이 먼저 끝났으면, 왼쪽 나머지 데이터 복사
+	else
+	{
+		while (leftIdx <= mid)
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+	}
+	
+	for (int i = 0; i < temp.size(); i++)
+		v[left + i] = temp[i];
+}
+
+void MergeSort(vector<int>& v, int left, int right)
+{
+	if (left >= right)
+		return;
+	int mid = (left + right) / 2;
+
+	MergeSort(v, left, mid); // 4
+	MergeSort(v, mid + 1, right); // 4
+
+	MergeResult(v, left, mid, right);
+}
+
 int main()
 {
-	BinarySearchTree bst;
+	vector<int> v;
 
-	bst.Insert(30);
-	bst.Print();
-	this_thread::sleep_for(1s);
-	
-	bst.Insert(10);
-	bst.Print();
-	this_thread::sleep_for(1s);
+	srand(time(0));
 
-	bst.Insert(20);
-	bst.Print();
-	this_thread::sleep_for(1s);
+	for (int i = 0; i < 50; i++)
+	{
+		int randValue = rand() % 100;
+		v.push_back(randValue);
+	}
 
-	bst.Insert(25);
-	bst.Print();
-	this_thread::sleep_for(1s);
+	MergeSort(v, 0, v.size() - 1);
 
-	bst.Delete(20);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Delete(10);
-	bst.Print();
-	this_thread::sleep_for(1s);
 }
